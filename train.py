9418a30ea6e3ff9954b4fe6fe4e7be02d86821d9
louis <sanicisspeedandfast@gmail.com>
2026-07-09 13:36:11 -0400

    starting to lay out the model

--- train.py
+++ train.py
@@ -1,3 +1,7 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
 # control the simulation with network
 # after one episode, update the NN
 # repeat 
@@ -7,4 +11,27 @@
 # weights are length 5
 # define our output: motor command
 
+class Network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(4, 64),
+            nn.ReLU(),
+            nn.Linear(64, 64),
+            nn.ReLU(),
+            nn.Linear(64, 2),
+        )
+    def forward(self, state):
+        return self.net(state)
+
+def main():
+    for iteration in range(10):
+        for episode in range(100):
+            while not done:
+                with torch.no_grad():
+                    action, log_prob= policy.sample(state)
+                next_state, reward, done = sim.step(action)
+                episode_log.append((state,action,reward))
+                state=next_state
+            batch.append(episode_log)
 
